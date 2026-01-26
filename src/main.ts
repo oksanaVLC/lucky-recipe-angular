3952ab6db4cf6e7d5382c8fd332b24bf2dc2137d
Oksana Golubeva <oksana@uoc.edu>
2026-01-26 15:12:56 +0100

    fix(router): enable scrollPositionRestoration for standalone app

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,24 @@
+/* Antes
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app';
 import { appConfig } from './app/app.config';
 
 bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
+*/
+
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
+import { AppComponent } from './app/app';
+import { appConfig } from './app/app.config';
+import { routes } from './app/app.routes';
+
+bootstrapApplication(AppComponent, {
+  ...appConfig,
+  providers: [
+    ...(appConfig.providers || []),
+    provideRouter(
+      routes,
+      withInMemoryScrolling({ scrollPositionRestoration: 'top', anchorScrolling: 'enabled' }),
+    ),
+  ],
+}).catch((err) => console.error(err));
