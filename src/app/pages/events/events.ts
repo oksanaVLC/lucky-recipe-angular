ad826e1ad9e54700bdf3827577a638fa7c99ddc9
Oksana Golubeva <oksana@uoc.edu>
2026-02-16 17:12:58 +0100

    otra vez slider

--- src/app/pages/events/events.ts
+++ src/app/pages/events/events.ts
@@ -1,14 +1,29 @@
-import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { CommonModule } from '@angular/common'; // <-- IMPORTANTE
+import {
+  AfterViewInit,
+  Component,
+  CUSTOM_ELEMENTS_SCHEMA,
+  ElementRef,
+  ViewChild,
+} from '@angular/core';
 
 @Component({
   selector: 'app-events',
   standalone: true,
-
-  imports: [],
+  imports: [CommonModule], // <-- aquí lo agregamos
   templateUrl: './events.html',
   styleUrls: ['./events.scss'],
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class EventsComponent {
-  text = 'Tu evento podría estar aquí...';
+export class EventsComponent implements AfterViewInit {
+  @ViewChild('swiperRef', { static: false }) swiperRef!: ElementRef;
+
+  images = ['assets/images/event.webp', 'assets/images/event1.webp', 'assets/images/turkey.webp'];
+
+  ngAfterViewInit(): void {
+    if (this.swiperRef) {
+      const swiperEl = this.swiperRef.nativeElement as any;
+      setTimeout(() => swiperEl.update(), 50);
+    }
+  }
 }
