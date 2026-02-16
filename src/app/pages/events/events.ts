4cfae68ceb4fcdfd211d7a86d7047986645e80f1
Oksana Golubeva <oksana@uoc.edu>
2026-02-16 16:58:53 +0100

    slider de eventos

--- src/app/pages/events/events.ts
+++ src/app/pages/events/events.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-events',
   standalone: true,
+
+  imports: [],
   templateUrl: './events.html',
   styleUrls: ['./events.scss'],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class EventsComponent {
   text = 'Tu evento podría estar aquí...';
